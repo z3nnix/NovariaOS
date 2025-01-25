9b377c63dae3696034dea1591af52bdc9b63b990
z3nnix <ponomarv-d@internet.ru>
2025-01-25 04:10:08 +0300

    core: dirvers/serial.h - fix warning

--- core/drivers/serial.h
+++ core/drivers/serial.h
@@ -5,10 +5,11 @@
 
 #define PORT 0x3f8 // COM1
 
+char read_serial();
 int init_serial();
 int serial_received();
-char read_serial();
 int is_transmit_empty();
 void write_serial(char a);
+void serial_print(const char* str);
 
 #endif // SERIAL_H
