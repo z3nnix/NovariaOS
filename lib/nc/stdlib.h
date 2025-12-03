2a5c5b005259ac28a01e700df494d4ebbca278d6
Zennix <zennix@tuta.io>
2025-12-03 13:33:22 +0300

    remove some trash(e.g useless poweroff function and lib/nc/time.h. update website and docs, remove exokernel mention

--- lib/nc/stdlib.h
+++ lib/nc/stdlib.h
@@ -2,20 +2,22 @@
 
 unsigned int randSeed = 12345;  // Initializing the seed for the pseudo-random number generator
 
-int rand(int min, int max) { // Function to generate a pseudo-random number in the range [min, max]
-    randSeed = randSeed * 1103515245 + 12345; // Linear congruential generator formula to update the seed
-    int randValue = (randSeed / 65536) % 32768; // Extracting the random number using the seed
+// generate a pseudo-random number in the range [min, max]
+int rand(int min, int max) {
+    randSeed = randSeed * 1103515245 + 12345;
+    int randValue = (randSeed / 65536) % 32768;
     
-    return min + (randValue % (max - min + 1)); // Mapping the generated value to the specified range [min, max]
+    return min + (randValue % (max - min + 1));
 }
 
-int srand(int min, int max, int seed) { // Function to generate a pseudo-random number in the range [min, max] with given seed [seed].
-    unsigned int randSeed = seed;  // Initializing the seed for the pseudo-random number generator
-    randSeed = randSeed * 1103515245 + 12345;  // Linear congruential generator formula to update the seed
+// generate a pseudo-random number in the range [min, max] with given seed [seed].
+int srand(int min, int max, int seed) {
+    unsigned int randSeed = seed;
+    randSeed = randSeed * 1103515245 + 12345;
 
-    int randValue = (randSeed / 65536) % 32768; // Extracting the random number using the seed
+    int randValue = (randSeed / 65536) % 32768;
     
-    return min + (randValue % (max - min + 1)); // Mapping the generated value to the specified range [min, max]
+    return min + (randValue % (max - min + 1));
 }
 
 void reverse(char* str, int length) {
