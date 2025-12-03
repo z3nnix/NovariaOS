778f7a517f113e2ecc28c49a4f4b464b2a528442
Zennix <zennix@tuta.io>
2025-12-03 13:35:45 +0300

    remove some trash(e.g useless poweroff function and lib/nc/time.h. update website and docs, remove exokernel mention

--- core/arch/acpi.c
+++
@@ -1,8 +0,0 @@
-void acpi_off() {
-    unsigned short port = 0x604;
-    unsigned short value = 0x2000;
-    asm volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
-    while (1) {
-        asm volatile ("hlt");
-    }
-} // https://t.me/novariaos/11
